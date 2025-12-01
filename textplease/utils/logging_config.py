05a8c85111f604f5cf75f166440390cc41cece96
Alina Lozovskaya <alinailozovskaya@gmail.com>
2025-12-01 02:29:57 +0100

    Add function docstring

--- textplease/utils/logging_config.py
+++ textplease/utils/logging_config.py
@@ -1,8 +1,8 @@
 import logging
 
 
-def configure_logging(level: int = logging.INFO, log_format: str = None) -> None:
-    """Configure logging"""
+def configure_logging(level: int = logging.INFO, log_format: str | None = None) -> None:
+    """Configure logging."""
     if log_format is None:
         log_format = "%(asctime)s - %(name)s - %(levelname)s - %(funcName)s:%(lineno)d - %(message)s"
 
